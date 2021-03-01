4754d36ea93442ab1289a942a246d3ceb61eb0a6
Edgardo Obregón <edraobdu@gmail.com>
2021-02-28 19:56:32 -0500

    Change deployment flow

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     python_requires='>=3.5'
 )
